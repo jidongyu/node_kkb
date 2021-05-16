8a72ce91dabbdd291e6bab07f2b09b70b600727b
jidongyu <jidy@sunmnet.com>
2021-05-17 01:07:31 +0800

    高级教程练习添加

---
+++ 高级课程练习/模块/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-17 01:01:48
+ * @LastEditTime: 2021-05-17 01:02:04
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \node\node_test\node_kkb\高级课程练习\模块\index.js
+ */
+require("mytest")
